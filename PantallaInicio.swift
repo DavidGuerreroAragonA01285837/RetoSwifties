35366c3294d6d85a7b00bd18f16bff7056423ddd
4ll3ssandro <a00839934@tec.mx>
2025-08-21 10:10:28 -0600

    Create PantallaInicio.swift

---
+++ PantallaInicio.swift
@@ -0,0 +1,18 @@
+//
+//  PantallaInicio.swift
+//  
+//
+//  Created by Alumno on 21/08/25.
+//
+
+import SwiftUI
+
+struct PantallaInicio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PantallaInicio()
+}
