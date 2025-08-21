7658f6be2a3ae430510a017336ee511601d68369
DavidGuerreroAragonA01285837 <a01285837@tec.mx>
2025-08-21 10:15:00 -0600

    Commit inicial
    Commit que contiene los archivos iniciales del desarrollo de la App en Swift. Contiene la carpeta de proyecto creada por XCode

---
+++ RetoSwifties/RetoSwifties/RetoSwiftiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoSwiftiesApp.swift
+//  RetoSwifties
+//
+//  Created by Alumno on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RetoSwiftiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
